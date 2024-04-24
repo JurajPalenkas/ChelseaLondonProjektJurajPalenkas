a15a1c865a44924573b4a6d0f833c361d5a089e5
JurajPalenkas <juraj.palenkas@student.ukf.sk>
2024-04-24 10:19:13 +0200

    nadizajnovanie fóra

--- forum.php
+++ forum.php
@@ -1,7 +1,10 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Chelsea London Projekt Juraj Palenkas - Fórum</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fórum</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="css/css.css">
 </head>
 <body>
@@ -9,7 +12,7 @@
     require 'header.php';
     ?>
 
-    <h1>Vitajte na fóre Chelsea London Projekt Juraj Palenkas</h1>
+    <h1>Fórum modrých trpiteľov</h1>
 
     <section id="forum">
         <?php
