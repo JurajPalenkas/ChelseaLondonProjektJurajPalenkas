0593df0912b983d5fb0e901ff4fa57a5c40b3dfa
JurajPalenkas <juraj.palenkas@student.ukf.sk>
2024-05-27 16:06:14 +0200

    fix

--- db.php
+++ db.php
@@ -6,9 +6,7 @@ $dbname = "db";
 
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    // nastavenie režimu výnimiek PDO
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Úspešne pripojený";
 } catch(PDOException $e) {
     echo "Pripojenie zlyhalo: " . $e->getMessage();
 }
